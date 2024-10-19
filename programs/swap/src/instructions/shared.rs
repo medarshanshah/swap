ec4292a969d2110fd4d3436ca26c1d56c96ded7a
Darshan <darshanshah.ds2017@gmail.com>
2024-10-19 18:30:43 +0000

    few comments added

--- programs/swap/src/instructions/shared.rs
+++ programs/swap/src/instructions/shared.rs
@@ -2,8 +2,8 @@ use anchor_lang::prelude::*;
 use anchor_spl::{token::{ transfer_checked, Mint, TransferChecked}, token_interface::{TokenAccount, TokenInterface}};
 
 pub fn transfer_tokens<'info>(
-    from: &InterfaceAccount<'info, TokenAccount>,
-    to: &InterfaceAccount<'info, TokenAccount>,
+    from: &InterfaceAccount<'info, TokenAccount>,   // InterfaceAccount is used for program to work on both old token program
+    to: &InterfaceAccount<'info, TokenAccount>,     // or the new token extensions program. With TokenAccount we can only work on either of those
     amount: &u64,
     mint: &InterfaceAccount<'info, Mint>,
     authority: &Signer<'info>,
@@ -20,6 +20,8 @@ pub fn transfer_tokens<'info>(
         token_program.to_account_info(), 
         transfer_accounts_options);
 
+    //Transferring tokens is done by calling the transfer_checked function of tokens program. 
+    // It has some logic to verify mints and a bunch of useful safety features
     transfer_checked(cpi_context, *amount, mint.decimals);
     Ok(())
 }
